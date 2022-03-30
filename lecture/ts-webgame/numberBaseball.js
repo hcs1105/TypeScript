b9b0eac4e01aae024526037760c8fe14f49b4c61
hcs1105 <hcs1105@gmail.com>
2022-03-31 03:45:47 +0900

    숫자 야구 만들기 진행

---
+++ lecture/ts-webgame/numberBaseball.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+var body = document.body;
+var candidate;
+var array = [];
+function chooseNumber() {
+    candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    array = [];
+    for (var i = 0; i < 4; i++) {
+        var chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+}
+chooseNumber();
+console.log(array);
+var result = document.createElement('h1');
+body.append(result);
