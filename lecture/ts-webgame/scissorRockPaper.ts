c76c3f0cee717b2a0ebf5962305df683e8290810
hcs1105 <hcs1105@gmail.com>
2022-04-02 21:52:09 +0900

    19강 수업 내용 및 가위바위보 게임 만들기 진행 중

--- lecture/ts-webgame/scissorRockPaper.ts
+++ lecture/ts-webgame/scissorRockPaper.ts
@@ -13,10 +13,13 @@ const rsp : RSP = {
 };
 
 const score = {
-  scissors : 1, 
-  rock : 0, 
-  paper : -1 
+  scissors : 1, // 가위의 점수
+  rock : 0, // 바위의 점수
+  paper : -1 // 보의 점수
 } as const;
+
+let interval: number;
+let point: number;
 
 function computerChoice(imgCoords : RSP[keyof RSP]) : keyof RSP {
   return (Object.keys(rsp) as ['scissors', 'rock', 'paper']).find((k) => rsp[k] === imgCoords)!;
@@ -24,16 +27,39 @@ function computerChoice(imgCoords : RSP[keyof RSP]) : keyof RSP {
 
 document.querySelectorAll('button').forEach(btn => {
   btn.addEventListener('click', function(this : HTMLButtonElement, e : Event){
-    const myChoice = this.textContent as keyof RSP;
+    clearInterval(interval);
+    setInterval(intervalMaker, 2000);
+
+    const myChoice = this.id as keyof RSP;
     const myScore = score[myChoice];
     const computerScore = score[computerChoice(imgCoords)]; // computerChoice(imgCoords)의 리턴값 : keyof RSP
     const diff = myScore - computerScore;
     if(diff === 0) {
       console.log('비겼습니다.');
-    } else if([-1,2].indexOf(diff)) {
+    } else if([-1,2].includes(diff)) {
       console.log('이겼습니다.');
+      point++;
     } else {
       console.log('졌습니다.');
+      point--;
     }
+    document.querySelector('#point').textContent = point;
   });
 });
+
+function intervalMaker() {
+  interval = setInterval(function(){
+    if(imgCoords === rsp.scissors) {
+      imgCoords = rsp.rock;
+    } else if(imgCoords === rsp.rock) {
+      imgCoords = rsp.paper;
+    } else {
+      imgCoords = rsp.scissors;
+    }
+    if(document.querySelector('#computer')) {
+      (document.querySelector('#computer') as HTMLDivElement).style.background = `url(images/srp.png) ${imgCoords} 0`;
+    }
+  }, 100);  
+}
+
+intervalMaker();
